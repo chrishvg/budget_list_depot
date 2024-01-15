b56c325cc39094c4b006863629a7babb11319be2
Christian Valdivieso <cjvaldivieso@gmail.com>
2024-01-14 20:38:33 -0500

    feature: add vuetify library

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="hello">
+    <v-btn color="success">Success</v-btn>
+    <v-btn color="error">Error</v-btn>
+    <v-btn color="warning">Warning</v-btn>
+    <v-btn color="info">Info</v-btn>
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
