120d0c8b84aba47b21f31e41315ac6d6dde922ee
Christian Valdivieso <cjvaldivieso@gmail.com>
2024-01-18 20:43:09 -0500

    feature: load file is added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,58 @@
 <template>
-  <div>
-    <button @click="scrapePage('https://quotes.toscrape.com/')">Scrapear Página</button>
-    <div v-if="scrapedData">
-      <h2>{{ scrapedData.title }}</h2>
-      <p>{{ scrapedData.description }}</p>
-    </div>
-  </div>
+  <v-row style="height: 100vh">
+    <v-col cols="6">
+      <v-file-input
+        v-model="files"
+        color="deep-purple-accent-4"
+        counter
+        label="File input"
+        placeholder="Select your file"
+        prepend-icon="mdi-paperclip"
+        variant="outlined"
+        style="height: 100vh"
+      >
+        <template v-slot:selection="{ fileNames }">
+          <template v-for="(fileName) in fileNames" :key="fileName">
+            <v-chip
+              color="deep-purple-accent-4"
+              label
+              size="small"
+              class="me-2"
+            >
+              {{ fileName }}
+            </v-chip>
+          </template>
+        </template>
+      </v-file-input>
+    </v-col>
+    <v-col cols="6">
+      <button @click="scrapePage('https://quotes.toscrape.com/')">Scrapear Página</button>
+      <div v-if="scrapedData">
+        <h2>{{ scrapedData.title }}</h2>
+        <p>{{ scrapedData.description }}</p>
+      </div>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
 import axios from 'axios'
+
 export default {
   data() {
     return {
-      scrapedData: null
+      scrapedData: null,
+      files: [],
     };
   },
+
+  watch: {
+    files: function(oldVal, newVal) {
+      console.log(oldVal)
+      console.log(newVal)
+    }
+  },
+
   methods: {
     scrapePage(url) {
       axios.get(url)
